6fa19d4e5e06dc3cda44ab9d78e655970b43b308
Your Name <you@example.com>
2021-12-17 09:31:37 +0800

    feat: 支持收集错误栈

--- lib/fluent/plugin/out_sentry.rb
+++ lib/fluent/plugin/out_sentry.rb
@@ -1,23 +1,26 @@
 require "fluent/plugin/output"
+require "sentry-ruby"
 
 module Fluent
   module Plugin
     class SentryOutput < Fluent::Plugin::Output
       Fluent::Plugin.register_output("sentry", self)
 
-      config_param :dsn, :string, :secret => true
-      config_param :title, :string, :default => 'test log'
+      config_param :dsn, :string
+      config_param :title, :string, :default => 'test'
       config_param :level, :enum, list: [:fatal, :error, :warning, :info, :debug], :default => 'info'
       config_param :environment, :string, :default => 'local'
-
+      config_param :type, :enum, list: [ :event, :exception], :default => :event
+      # for event
       config_param :user_keys, :array, :default => [], value_type: :string
       config_param :tag_keys, :array, :default => [], value_type: :string
       config_param :keys, :array, :default => [], value_type: :string
+      # for exception
+      config_param :lang, :string, :default => 'php'
 
 
       def initialize
         require 'time'
-        require "sentry-ruby"
 
         super
       end
@@ -27,7 +30,6 @@ module Fluent
 
         config = Sentry::Configuration.new
         config.dsn = @dsn
-        config.environment = @environment
         config.send_modules = false
         config.transport.timeout = 5
 
@@ -39,15 +41,29 @@ module Fluent
           begin
             event = Sentry::Event.new(configuration: @client.configuration)
 
-            event.message = @title
-            event.level = record['level'] || @level
+            event.message = record['message'] || @title
+            event.level = (record['level'] || @level).downcase
+            event.environment = record['env'] || @environment
 
-            event.user = record.select{ |key| @user_keys.include?(key) }
-            event.extra = @keys.length() > 0 ? record.select{ |key| @keys.include?(key) } : record
-            event.contexts = {'data' => { origin_data: record }}
-            event.tags = event.tags.merge({ :platform => tag })
-              .merge({ :timestamp => Time.at(time).strftime('%Y-%m-%d %H:%M:%S') })
-              .merge(record.select{ |key| (@tag_keys + @user_keys).include?(key) })
+            if @type === :event
+              event.user = record.select{ |key| @user_keys.include?(key) }
+              event.extra = @keys.length() > 0 ? record.select{ |key| @keys.include?(key) } : record
+              event.contexts = {'data' => { origin_data: record }}
+              event.tags = event.tags.merge({ :platform => tag })
+                .merge({ :timestamp => Time.at(time).strftime('%Y-%m-%d %H:%M:%S') })
+                .merge(record.select{ |key| (@tag_keys + @user_keys).include?(key) })
+            elsif @type === :exception
+              event = event.to_hash
+              event['exception'] = Sentry::CustomExceptionInterface.new(
+                type: 'FatalThrowableError',
+                message: record['message'],
+                stacktrace: Sentry::StacktraceInterface.new(frames: [Sentry::CustomStacktraceFrame.new(
+                  filename: 'index' + '.' + @lang,
+                  context_line: record['message'],
+                  post_context: record['stack']
+                )])
+              ).to_hash
+            end
 
             @client.send_event(event)
           rescue => e
@@ -63,6 +79,47 @@ module Fluent
       def formatted_to_msgpack_binary
         true
       end
+    end
+  end
+end
+
+module Sentry
+  class CustomExceptionInterface < Sentry::Interface
+    attr_reader :type
+    attr_reader :message
+
+    SKIP_INSPECTION_ATTRIBUTES = [:@stacktrace]
+
+    def initialize(type:, message:, stacktrace: nil)
+      @type = type
+      @value = message
+      @stacktrace = stacktrace
+    end
+
+    def to_hash
+      data = super
+      data[:stacktrace] = data[:stacktrace].to_hash if data[:stacktrace]
+      data
+    end
+  end
+
+  class CustomStacktraceFrame < Sentry::Interface
+    attr_accessor :filename, :pre_context, :context_line, :post_context, :lineno
+
+    def initialize(filename:, context_line:, pre_context: '', post_context: '', lineno: 1)
+      @filename = filename
+      @context_line = context_line
+      @pre_context = pre_context.split("\n")
+      @post_context = post_context.split("\n")
+      @lineno = lineno
+    end
+
+    def to_hash(*args)
+      data = super(*args)
+      data.delete(:pre_context) unless pre_context && !pre_context.empty?
+      data.delete(:post_context) unless post_context && !post_context.empty?
+      data.delete(:context_line) unless context_line && !context_line.empty?
+      data
     end
   end
 end
